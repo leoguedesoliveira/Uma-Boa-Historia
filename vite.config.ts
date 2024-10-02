89f2671da8ac90eff8f3cb766379bf2022c8f685
leoguedesoliveira <=>
2024-10-02 20:09:17 -0300

    Ajuste na estrutura de pastas para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Uma-Boa-Historia"
+  base: "/Uma-Boa-Historia",
+  publicDir: 'public'
 })
